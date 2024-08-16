505124729bc38468f741551a6e19e8dcc6f2b610
allen-li1231 <supermrli@hotmail.com>
2024-08-16 16:00:56 +0800

    add evaluate datasets for testing the upper bond of the idea

--- submit_get_refiner_teacher_data.py
+++ submit_get_refiner_teacher_data.py
@@ -5,12 +5,21 @@ os.environ["HF_HUB_OFFLINE"] = "1"
 os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
 # NUM_GPUS = 4
 
-TASKS = [
+TRAIN_TASKS = [
     "musique_train",
     "arc_c_train",
     "triviaqa_train",
-    "hotpotqa_train",
+    # "hotpotqa_train",
     "pubhealth_train",
+]
+
+EVAL_TASKS = [
+    # "musique_dev",
+    # "popqa",
+    # "triviaqa",
+    # "hotpotqa_dev_distractor",
+    "2wiki_dev",
+    "arc_c",
 ]
 
 downstream_model_names = (
@@ -33,7 +42,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Get evaluation for train and evaluation task")
 
     parser.add_argument(
-        "--train_dir", type=str, default=None, help="name to directory containing training data"
+        "--task_dir", type=str, default=None, help="name to directory containing training data"
     )
     parser.add_argument(
         "--top_n", type=int, default=10, help="number of top retrieval"
@@ -46,28 +55,28 @@ def parse_args():
 def run_task(
         task,
         top_n=10,
-        train_dir=None):
+        task_dir=None):
 
-    if train_dir is None:
-        train_dir = f"./train_data/"
+    if task_dir is None:
+        task_dir = f"./train_data/"
 
     matched_file_name = None
-    for file_name in os.listdir(train_dir):
+    for file_name in os.listdir(task_dir):
         if file_name.startswith(task):
             matched_file_name = file_name
-        if file_name == f"{task}_teacher_models.jsonl":
+        if f"{task}_teacher_models_top{top_n}.jsonl" in file_name:
             print(f"Continue recording with {file_name}")
             break
 
     if matched_file_name is None:
-        raise IndexError(f"Warning: {task} task not evaluated, maybe file not found in", train_dir)
+        raise IndexError(f"Warning: {task} task not evaluated, maybe file not found in", task_dir)
 
-    file_path = os.path.abspath(os.path.join(train_dir, matched_file_name))
-    output_path = os.path.abspath(os.path.join(train_dir, f"{task}_teacher_models.jsonl"))
+    file_path = os.path.abspath(os.path.join(task_dir, matched_file_name))
+    output_path = os.path.abspath(os.path.join(task_dir, f"{task}_teacher_models_top{top_n}.jsonl"))
     model_list = zip(downstream_model_names, downstream_inference_name)
 
     for model, inference in model_list:
-        print(f"Evaluating {file_path} using {inference}")
+        print(f"Run refiner teacher models using {file_path} on {inference}")
         os.system(f"""
 python ./get_refiner_teacher_data.py \
     --model_name_or_path {model} \
@@ -83,9 +92,13 @@ python ./get_refiner_teacher_data.py \
 if __name__ == '__main__':
     args = parse_args()
 
-    for task in TASKS:
+    lst_tasks = TRAIN_TASKS
+    if isinstance(args.task_dir, str) and "eval_data" in args.task_dir:
+        lst_tasks = EVAL_TASKS
+
+    for task in lst_tasks:
         run_task(task=task,
                 top_n=args.top_n,
-                train_dir=args.train_dir)
+                task_dir=args.task_dir)
 
-    # python ./submit_get_refiner_teacher_data.py --top_n 10
+    # python ./submit_get_refiner_teacher_data.py --top_n 10 --task_dir ./eval_data
