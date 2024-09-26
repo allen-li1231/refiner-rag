9608c4ab39a639b40da9bdbe7ffb9860c9d74bc1
allen-li1231 <supermrli@hotmail.com>
2024-09-26 17:35:11 +0800

    cleanup teacher model ballots after loading and before saving datasets

--- consolidate_teachers.py
+++ consolidate_teachers.py
@@ -6,6 +6,14 @@ from utils import regex_section
 from submit_get_refiner_teacher_data import TRAIN_TASKS, EVAL_TASKS, downstream_inference_name
 
 
+def empty_ballot(ctxs):
+    for ctx in ctxs:
+        if "ballot" in ctx:
+            del ctx["ballot"]
+
+    return ctxs
+
+
 def common_str_idx(s1: str, s2: str):
     """
     Find longest common substring and return the corresponding index in s1
@@ -169,7 +177,7 @@ def generate_exemplar(df, teacher_names, min_valid_quote_len=15):
     n_majority = (n_teachers + 1) // 2
     n_empties = 0
 
-    d_contexts = df["ctxs"]
+    lst_context = df["ctxs"]
     for teacher_name in teacher_names:
         lst_quotes = parse_quotes(df[teacher_name])
         if lst_quotes == '':
@@ -187,10 +195,10 @@ def generate_exemplar(df, teacher_names, min_valid_quote_len=15):
                 # skip data as number of valid outputs are insufficient
                 return None
         else:
-            vote_quotes(d_contexts, lst_quotes, teacher_name)
+            vote_quotes(lst_context, lst_quotes, teacher_name)
 
     # get most voted quote text and the corresponding voters
-    for context in d_contexts:
+    for context in lst_context:
         quote, voters = extract_major_quote(
             context["text"], context["ballot"], n_majority, return_voters=True)
 
@@ -202,11 +210,11 @@ def generate_exemplar(df, teacher_names, min_valid_quote_len=15):
         context["quote"] = quote
         context["voters"] = voters
 
-    reassign_section(d_contexts, n_majority)
+    reassign_section(lst_context, n_majority)
 
     # concatenate quotes, sections along with the titles
     lst_exemplar = []
-    for context in d_contexts:
+    for context in lst_context:
         if "quote" not in context:
             continue
 
@@ -227,21 +235,24 @@ if __name__ == '__main__':
     top_n = 10
 
     for task in EVAL_TASKS:
-        df_eval_data: pd.DataFrame = pd.read_json(f"eval_data/{task}_teacher_models_top{top_n}.jsonl", lines=True)
-
+        df_eval_data: pd.DataFrame = pd.read_json(f"eval_data/teacher_models/{task}_exemplar_top{top_n}.jsonl", lines=True)
+        # ensure everything starts from zero
+        df_eval_data["ctxs"].apply(empty_ballot)
+        df = df_eval_data.iloc[0]
+        teacher_names = downstream_inference_name
         df_eval_data["exemplar"] = df_eval_data.progress_apply(generate_exemplar, args=(downstream_inference_name,), axis=1)
         # remove dirty exemplars
         df_eval_data = df_eval_data[~df_eval_data["exemplar"].isna()]
-
-        df_eval_data.to_json(f"eval_data/{task}_teacher_models_top{top_n}.jsonl", lines=True, orient="records")
+        df_eval_data["ctxs"].apply(empty_ballot)
+        df_eval_data.to_json(f"eval_data/{task}_exemplar_top{top_n}.jsonl", lines=True, orient="records")
         # df_eval_data["exemplar"][~df_eval_data["exemplar"].isna() & (df_train_data["exemplar"].str.len() == 0)]
-    
-    for task in TRAIN_TASKS:
-        df_train_data: pd.DataFrame = pd.read_json(f"train_data/{task}_teacher_models_top{top_n}.jsonl", lines=True)
 
+    for task in TRAIN_TASKS:
+        df_train_data: pd.DataFrame = pd.read_json(f"train_data/{task}_exemplar_top{top_n}.jsonl", lines=True)
+        df_train_data["ctxs"].apply(empty_ballot)
         df_train_data["exemplar"] = df_train_data.progress_apply(generate_exemplar, args=(downstream_inference_name,), axis=1)
         # remove dirty exemplars
         df_train_data = df_train_data[~df_train_data["exemplar"].isna()]
-
-        df_train_data.to_json(f"train_data/{task}_teacher_models_top{top_n}.jsonl", lines=True, orient="records")
+        df_train_data["ctxs"].apply(empty_ballot)
+        df_train_data.to_json(f"train_data/{task}_exemplar_top{top_n}.jsonl", lines=True, orient="records")
         # df_train_data["exemplar"][~df_train_data["exemplar"].isna() & (df_train_data["exemplar"].str.len() == 0)]
